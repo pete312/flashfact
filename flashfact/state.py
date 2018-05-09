14597046fc51c95f4f1fec94d30b839b74043c53
Pete Moore <1petemoore@gmail.com>
2018-05-09 11:01:13 -0500

    changed State to AppState singleton

---
+++ flashfact/state.py
@@ -0,0 +1,19 @@
+# singleton pattern to store application state
+class AppState:
+    class __State:
+            
+        def __str__(self):
+            return repr(self) + self.val
+    instance = None
+    def __init__(self):
+        if not State.instance:
+            State.instance = State.__State()
+        
+            
+    def __setattr__(self, name, val):
+        setattr(self.instance, name, val)
+        
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+        
+    
