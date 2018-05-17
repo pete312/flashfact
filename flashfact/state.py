04e64aad2c168abe037e78159b8905a790471620
Pete Moore <1petemoore@gmail.com>
2018-05-17 15:29:52 -0500

    added get and set methods for attributes.

--- flashfact/state.py
+++ flashfact/state.py
@@ -1,11 +1,26 @@
 # singleton pattern to store application state
 
 
+debug = False # set to true to debug setting overrides 
+
+
 class AppState:
     class __State:
             
         def __str__(self):
             return repr(self) + self.val
+            
+        def get(self, attr, default):
+            value = getattr( AppState.instance, attr, default) 
+            if debug and value != default:
+                    print("appstate overrides %s of %s with %s" % (attr , default, value))
+            return getattr( AppState.instance, attr, default)
+        
+        def set(self, attr, value): 
+            setattr(AppState.instance, attr, value)
+            
+            
+            
     instance = None
     def __init__(self):
         if not AppState.instance:
@@ -18,4 +33,6 @@ class AppState:
     def __getattr__(self, name):
         return getattr(self.instance, name)
         
-    
+    def save(self):
+        '''save state into ~/.<appname>rc'''
+        open()
